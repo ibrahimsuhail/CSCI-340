e9ee9988534f57c01c5bb4314c0899319c902bfc
ibrahimsuhail <45448860+ibrahimsuhail@users.noreply.github.com>
2020-03-25 17:30:25 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 //
 //  main.cpp
-//  mtrand
-//
+//  multi threaded random number generator
+//  project 2
 //  Created by ibrahim suhail on 3/18/20.
 //  Copyright © 2020 ibrahim suhail. All rights reserved.
 //
@@ -13,66 +13,85 @@
 #include <algorithm>
 #include <pthread.h>
 #include <stdio.h>
-/*
-specs:
-1) generate 10k random numbers from 1-10,000 [x]
-2) sort using and then output the min, max from the sample [x]
-3) find the min of the 10k samples using non MT way. [x]
-4) find the min using MT way. (10,000/5) => 2,000/5 => 400/5 => 80 per thread. Could even stop at 400
-*/
-
+// generate 5 * 20 threads
 #define NUM_THREADS 5
+#define NUM_SUBTHREADS 20
 #define cap 10000
 using namespace std;
 int num;
-int min_th[NUM_THREADS] = { 0 };
+// this array stores the minimum element of each subthread
+int min_th[NUM_SUBTHREADS] = { 0 };
 int minArray[cap];
 vector<int> myVector;
 int threadNumber = 0;
-  
+  // generate 10,000 random numbers from 0 - 10,000 and place in minArray
 void getRands(){
   srand(time(NULL));
   for (int i = 0; i < cap; i++)
     minArray[i] = (rand() % cap);
 }
+// find the minimum element without multithreading
+void nonMT(){
+  getRands();
+    // add each element of minArray to vector
+  for (int i = 0; i < cap; i++)
+  myVector.push_back(minArray[i]);
+    // sort the vector from smallest to largest element
+  sort(myVector.begin(), myVector.end());
+  int min1 = myVector[0];
+  int max1= myVector[cap-1];
+  cout <<"min nonMT: "<< min1 <<endl << "max nonMT: " << max1 << endl;
+}
+// finds the minimum element in each array of random numbers
 void *minimum(void *arg){
     getRands();
     int num = threadNumber++;
-    int min1 = 0;
-    for (int i = num * (cap / NUM_THREADS); i < (num + 1) * (cap / NUM_THREADS); i++) {
+    int min1 = minArray[0];
+    for (int i = 0 ; i < cap; i++) {
         if (minArray[i] < min1)
             min1 = minArray[i];
     }
     min_th[num] = min1;
     pthread_exit(0);
 }
-void nonMT(){
-  getRands();
-  for (int i = 0; i < cap; i++)
-  myVector.push_back(minArray[i]);
-  sort(myVector.begin(), myVector.end());
-  int min1 = myVector[0];
-  int max1= myVector[cap-1];
-  cout <<"min nonMT: "<< min1 <<endl << "max nonMT: " << max1 << endl;
+// each subthread will call the minimum function
+void *subth(void *arg){
+    int i;
+    // array of threads
+    pthread_t threads[NUM_SUBTHREADS];
+    // create 20 subthreads and join them
+    // each thread will call the minimum function to find the minimum element in each array of random numbers
+    for (i = 0; i < NUM_SUBTHREADS; i++)
+        pthread_create(&threads[i], NULL, minimum, (void*)NULL);
+    for (i = 0; i < NUM_SUBTHREADS; i++)
+        pthread_join(threads[i], NULL);
+    pthread_exit(0);
 }
+// gets the minimum element from array containing the minimum elements of each thread
 void minMT(){
-       int MIN = 0;
-       int i;
-       pthread_t threads[NUM_THREADS];
-         for (i = 0; i < NUM_THREADS; i++)
-           pthread_create(&threads[i], NULL, minimum, (void*)NULL);
-       for (i = 0; i < NUM_THREADS; i++)
-           pthread_join(threads[i], NULL);
-       for (i = 0; i < NUM_THREADS; i++) {
-           if (min_th[i] < MIN)
-               MIN = min_th[i];
-       }
-       
-       cout<< "Minimum Element using multiple threads: " << MIN << endl;
+    int i;
+    pthread_t threads[NUM_THREADS];
+    // create 5 threads and join them
+    // each thread will call subth() to create 20 subthreads
+    for (i = 0; i < NUM_THREADS; i++)
+        pthread_create(&threads[i], NULL, subth, (void*)NULL);
+    for (i = 0; i < NUM_THREADS; i++)
+        pthread_join(threads[i], NULL);
+    // sort array of minimum values from threads
+    int j = sizeof(min_th)/sizeof(min_th[0]);
+    sort(min_th, min_th+j);
+    /*                                          uncomment to see every value of array
+    for (i = 0; i < NUM_SUBTHREADS ; i++)
+    cout<< min_th[i] << ", ";
+    */
+    int MIN = min_th[0];
+    cout << "minimum element using multithreading: " << MIN << endl;
 }
-// Driver code
+
 int main(){
+    // find minimum element without multithreading
     nonMT();
+    // find minimum element with multithreading
     minMT();
     return 0;
 }
